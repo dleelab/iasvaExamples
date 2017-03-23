fc62f7de0961554113e888c71d5ae7a99c973978
dleelab <dustorm@gmail.com>
2017-03-23 14:09:32 -0400

    new format

---
+++ R/Lawlor_Islet_scRNAseq_Annotations.R
@@ -0,0 +1,11 @@
+#' Human islet single cell RNA-Seq data from Lawlor et. al. (2017)
+#'
+#' Annotations describing the islet samples and experimental settings.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Lawlor_Islet_scRNAseq_Annotations
+#' @usage data(Lawlor_Islet_scRNAseq_Annotations)
+#' @format A data frame with 638 rows and 26 variables
+NULL
---
+++ R/Lawlor_Islet_scRNAseq_Read_Counts.R
@@ -0,0 +1,11 @@
+#' Human islet single cell RNA-Seq data from Lawlor et. al. (2017)
+#'
+#' Gene-level read counts from the single cell RNA sequencing experiment of Lawlor et. al. (2017)
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Lawlor_Islet_scRNAseq_Read_Counts
+#' @usage data(Lawlor_Islet_scRNAseq_Read_Counts)
+#' @format A data frame with 26616 rows and 638 variables
+NULL
