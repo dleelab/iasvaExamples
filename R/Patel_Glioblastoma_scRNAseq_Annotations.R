9e908eaba2fe082bfd7adedb1fbbd878ca8149ae
dleelab <dustorm@gmail.com>
2017-04-19 17:32:32 -0400

    Patel Glioblastoma data added

---
+++ R/Patel_Glioblastoma_scRNAseq_Annotations.R
@@ -0,0 +1,11 @@
+#' Human Glioblastoma Single Cell RNA-Seq Data from Patel et. al. (2014)
+#'
+#' Annotations describing the samples and experimental settings.
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Patel_Glioblastoma_scRNAseq_Annotations
+#' @usage data(Patel_Glioblastoma_scRNAseq_Annotations)
+#' @format A data frame with 434 rows and 3 variables
+NULL
---
+++ R/Patel_Glioblastoma_scRNAseq_Read_Counts.R
@@ -0,0 +1,11 @@
+#' Human Glioblastoma Single Cell RNA-Seq Data from Patel et. al. (2014)
+#'
+#' Gene-level read counts from the single cell RNA sequencing experiment of Patel et. al. (2014)
+#'
+#'
+#' @docType data
+#' @keywords datasets
+#' @name Patel_Glioblastoma_scRNAseq_Read_Counts
+#' @usage data(Patel_Glioblastoma_scRNAseq_Read_Counts)
+#' @format A data frame with 25415 rows and 434 variables
+NULL
